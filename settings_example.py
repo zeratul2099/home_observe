cb759b5039f49cff7e7f2d2886ecdf36016e07c1
Marko <zeratul@posteo.de>
2016-09-12 20:03:32 +0200

    comment fix

--- settings_example.py
+++ settings_example.py
@@ -8,7 +8,7 @@ nma_api_key = ''
 # limit of a host must be offline/away to be notified again when comming online. In minutes
 last_seen_delta = 15
 
-# also notify, go offline (not seen in the <last_seen_delta> minutes
+# also notify if device goes offline (not seen in the <last_seen_delta> minutes)
 notify_offline = True
 
 # database for logging

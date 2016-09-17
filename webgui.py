b55f8d711f3d92fe38fab3e80f9fee723e4eff4b
Marko Krause <zeratul@posteo.de>
2016-09-17 17:22:12 +0200

    added css

--- webgui.py
+++ webgui.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 from sqlalchemy import desc
 from common import get_host_shortname, get_database
 app = Flask(__name__)
@@ -9,7 +9,9 @@ def main():
     log = get_database()
     select = log.select().order_by(desc(log.c.timestamp))
     rows = select.execute()
-    result = '<html><body><table>'
+    result = '<html><head>'
+    result += '<link rel="stylesheet" type="text/css" href="/static/styles.css" >'
+    result += '</head><body><div id="content_container" class="content"><table>'
     for row in rows.fetchall():
         entry = dict(
             shortname = get_host_shortname(row.hostname),
@@ -21,9 +23,12 @@ def main():
         result += '<tr>'
         result += ('<td>{shortname:20s}</td><td>{status}</td><td>{timestamp}</td><td>{ipv4:15}</td><td>{ipv6}</td>'.format(**entry))
         result += '</tr>'
-    result += '</table></body></html>'
+    result += '</table></div></body></html>'
     return result
 
+
+with app.test_request_context():
+    url_for('static', filename='styles.css')
 
 if __name__ == '__main__':
     app.run()
